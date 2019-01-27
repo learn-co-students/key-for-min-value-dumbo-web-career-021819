29ffa7c464ba27b427e57182e2068d65f3c88809
Jo K <joel.kohn42@gmail.com>
2019-01-27 19:49:39 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,17 @@
 
 def key_for_min_value(name_hash)
 
+  lowest = nil
+  item = nil
+  name_hash.each do |key, value|
+  
+    if lowest == nil 
+      lowest = value
+      item = key
+    elsif value < lowest
+      lowest = value
+      item = key
+    end 
+  end 
+  item
 end
